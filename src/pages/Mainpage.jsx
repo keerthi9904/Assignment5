8240c39c3df3474894c20e697f8e9680291b466d
keerthi9904 <pvslk9904@gmail.com>
2025-04-17 12:04:17 +0530

    moved sidebar to left

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,15 +1,18 @@
+// import React, {useState} from "react";
 import Meals from "../components/Meals.jsx";
 import ScarcitySidebar from "../components/ScarcitySidebar.jsx";
 import ChatbotGreeting from "../components/ChatbotGreeting.jsx";
+// import Header from "../components/Header.jsx";
 
 function Mainpage() {
+
   return (
     <div style={{ display: "flex", height: "100vh" }}>
-      <div style={{ flex: 4, overflowY: "auto" }}>
-        <Meals />
-      </div>
-      <div style={{ flex: 1, borderLeft: "1px solid #ccc", backgroundColor: "#f9f9f9" }}>
+      <div style={{ flex: 1, borderRight: "1px solid #ccc", backgroundColor: "#f9f9f9" }}>
         <ScarcitySidebar />
+      </div>
+      <div style={{ flex: 4, overflowY: "auto" }}>
+          <Meals />
       </div>
       <ChatbotGreeting />
     </div>
